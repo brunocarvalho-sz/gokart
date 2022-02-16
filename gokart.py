84d483a7a0c6ee2d5e4adac2b34ff77516af5fae
brunocarvalho-sz <99615538+brunocarvalho-sz@users.noreply.github.com>
2022-02-16 18:34:03 -0300

    Update gokart.py
    Refactor do código principal

--- gokart.py
+++ gokart.py
@@ -1,30 +1,40 @@
 # import pdb
 # pdb.set_trace()
 
-# filename = 'Data/Export Bateria 5.csv'
-filename = 'Data/Export Bateria 7.csv'
-arquivo = open(filename, 'r', encoding='utf-8')  #read mode and forced UTF-8
-linhas = arquivo.readlines()
-arquivo.close()
 
-indices = [19, 20, 21, 22, 25]
+def tabela_dados():
+    # filename = 'Data/Export Bateria 5.csv'
+    filename = 'Data/Export Bateria 7.csv'
+    arquivo = open(filename, 'r', encoding='utf-8')  #read mode and forced UTF-8
+    linhas = arquivo.readlines()
+    arquivo.close()
 
-tabela = [linha.split(',') for linha in linhas]
-# tabela = []
-# for linha in linhas:
-    # aux = linha.split(',')
-    # tabela.append(aux)
+    indices = [19, 20, 21, 22, 25]
 
-# tabelavoltas = [[line[id] for id in indices] for line in tabela if len(line)==len(tabela[0])]
-tabelavoltas = []
-for linhatabela in tabela:
-    if len(linhatabela) != len(tabela[0]):
-        continue
-    
-    temp = []
-    for id in indices:
-        temp.append(linhatabela[id])
-    tabelavoltas.append(temp)
+    tabela = [linha.split(',') for linha in linhas]
+    # tabela = []
+    # for linha in linhas:
+        # aux = linha.split(',')
+        # tabela.append(aux)
 
-for volta in tabelavoltas:
-    print( str(volta).replace('[' , '').replace(']', '') )
+    # tabelavoltas = [[line[id] for id in indices] for line in tabela if len(line)==len(tabela[0])]
+    tabelavoltas = []
+    for linhatabela in tabela:
+        if len(linhatabela) != len(tabela[0]):
+            continue
+        
+        temp = []
+        for id in indices:
+            temp.append(linhatabela[id])
+        tabelavoltas.append(temp)
+
+    return tabelavoltas
+
+def print_tabela(tabelavoltas):
+    for volta in tabelavoltas:
+        print( str(volta).replace('[' , '').replace(']', '') )
+
+# O name é igual a mais quando você executa no cmd
+if __name__ == '__main__':  # python nomedoaquivo.py
+    teste = tabela_dados()
+    print_tabela(teste)
